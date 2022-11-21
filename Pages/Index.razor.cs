f253ddcce37f0858292a80435864d9cfb1fb9fef
dugalatortuga <gab2diegor@gmail.com>
2022-11-21 15:07:14 +0100

    qr generator

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,32 @@
+﻿using MudBlazor;
+using QRCoder;
+
+namespace QRGenerator.Pages
+{
+    public partial class Index  //partial means that Index Class is part of index
+    {
+        MudForm urlSubmitForm;
+        public string SubmittedUrl { get; set; }
+        public string QRCodeText { get; set; }
+
+        private async Task SubmitUrl()
+        {
+            await urlSubmitForm.Validate();
+            if (urlSubmitForm.IsValid)
+                GenerateQRCode();
+        }
+
+        protected void GenerateQRCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(SubmittedUrl, QRCodeGenerator.ECCLevel.Q); // generating img
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qRCodeData); // saving img into a bitmap
+            byte[] qrCodeAsByteArr = qrCode.GetGraphic(30); // saving img into a byte array
+
+            var ms = new MemoryStream(qrCodeAsByteArr);
+
+            QRCodeText = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+            SubmittedUrl = String.Empty;
+        }
+    }
+}
